cc5bbc9e9f1940fcc36d33efa4a3a20dd754a34c
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-06-15 11:17:40 +0300

    проходимся в цикле по массиву $_COOKIE

--- cookie/getcookie.php
+++ cookie/getcookie.php
@@ -1,5 +1,9 @@
 <?php
 
 if (isset($_COOKIE['flavor'])) {
-  echo "You ate a {$_COOKIE['flavor']} cookie";
+  echo "You ate a {$_COOKIE['flavor']} cookie<br>";
+}
+
+foreach ($_COOKIE as $cookie_name => $cookie_value) {
+  echo "$cookie_name = $cookie_value <br>";
 }
--- cookie/setcookie.php
+++ cookie/setcookie.php
@@ -1,5 +1,5 @@
 <?php
 
-setcookie('flavor', 'chocolate chip', time()+60);
-
+setcookie('flavor', 'chocolate chip');
+setcookie('next_station', 'Тимирязевская');
 echo '<a href="getcookie.php">link</a>';
